87d2d722395a961256a870e90699982783d2fa01
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-10-11 01:05:30 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,35 +1,18 @@
-# Name Mangler
-
+# One function will do only one thing at a time
+# Declare global name variable
 @name = "Johanna Jackson"
 
-# Mangle the name by reversing it or borgifying it
-def modified_name(choose)
-  # Split the name into first and last in an array
-  split_name = @name.split
-  #reversed_twice = split_name.reverse.reverse
-  #mangled_name = reversed_twice << "Mangled"
-  #return mangled_name * 2
-  # Reverse the array of name components so the array is [last, first]
-  reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
-  new_name1 = reversed_name_components.join(' ')
-  # Store the mangled name
-  @name = new_name1
-  puts new_name1
-  if choose
-    # Split the name into last and first in an array
-    split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
-    reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
-    reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
-    new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
-    @name = new_name2
-  end
-  return @name
+# reversing from (first name, last name) to (last name, first name)
+def reversing_name(name)
+  split_name = name.split
+  reversing_first_lastname = split_name.reverse
+  new_reversed_name = reversing_first_lastname.join(' ')
+  name = new_reversed_name
 end
+puts reversing_name(@name)
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+# appending 'Borg' to the last of the full name 
+def append_name(addname)
+  addname = "New borg name: " + reversing_name(addname) << " Borg "
+end
+puts add_name(@name)
